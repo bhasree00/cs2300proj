3020a5f2a46b72b2fc587702ca83daba357df2bf
Bharat Sreekrishnavilas <bhasree00@gmail.com>
2020-12-17 13:02:39 -0600

    round 2 commits

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,10 @@ from flask import Flask, render_template, request, flash
 from flask_sqlalchemy import SQLAlchemy
 #from flask_bootstrap import Bootstrap
 
-from flask_wtf import FlaskForm
-from wtforms import SubmitField, SelectField, RadioField, HiddenField, StringField, IntegerField, FloatField
-from wtforms.validators import InputRequired, Length, Regexp, NumberRange
-from datetime import date
+#from flask_wtf import FlaskForm
+#from wtforms import SubmitField, SelectField, RadioField, HiddenField, StringField, IntegerField, FloatField
+#from wtforms.validators import InputRequired, Length, Regexp, NumberRange
+#from datetime import date
 
 app = Flask(__name__)
 app.config.from_object('config')
